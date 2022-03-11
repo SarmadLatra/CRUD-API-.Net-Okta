63d47660144807021e3c299bdcb5b6d82cdadc1e
Sarmad Saeed <sarmad.saeed@bp.com>
2022-03-11 14:26:02 +0500

    Add CustomerInfoEndpoint

---
+++ CRUD API/Controllers/CustomerController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CRUD_API.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomerController : ControllerBase
+    {
+        [HttpGet("GetCustomerInformation")]
+        public IActionResult Index()
+        {
+            return Ok(new { CustomerName = "Sam", Email = "sam14@gmail.com", AccountBalance = "150000" });
+        }
+    }
+}
